4eedafb535d804bd0d531172cf2e3feccab24c55
Javier <javivaz2001@gmail.com>
2023-05-01 16:51:59 +0200

    Created spring api project

---
+++ tfgs/src/main/java/cbd/vazquez/tfgs/TfgsApplication.java
@@ -0,0 +1,13 @@
+package cbd.vazquez.tfgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfgsApplication.class, args);
+	}
+
+}
---
+++ tfgs/src/test/java/cbd/vazquez/tfgs/TfgsApplicationTests.java
@@ -0,0 +1,13 @@
+package cbd.vazquez.tfgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
