d452eeeeb5e23d0392a16efadea023ef7e7390e6
javiervaz01 <javivaz2001@gmail.com>
2023-05-04 01:49:03 +0200

    Creation of first classes react

---
+++ cbd-front/src/pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+function HomePage () {
+  return (
+    <p>Página de Vázquez de tfgs</p>
+  );
+};
+
+export default HomePage;
